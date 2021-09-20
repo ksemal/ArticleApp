4c9eb96d6ce12e8e9b350d6dd2bbe5b44b81cfa5
Ksenia Malyshev <starkcenia@gmail.com>
2021-09-20 14:44:28 -0400

    added pagination

--- app/src/main/java/com/example/articleapp/NewsFragment.kt
+++ app/src/main/java/com/example/articleapp/NewsFragment.kt
@@ -5,9 +5,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.articleapp.data.util.Resource
 import com.example.articleapp.databinding.FragmentNewsBinding
 import com.example.articleapp.presentation.adapter.NewsAdapter
@@ -19,6 +21,10 @@ class NewsFragment : Fragment() {
     private lateinit var fragmentNewsBinding: FragmentNewsBinding
     private var country = "us"
     private var page = 1
+    private var isScrolling: Boolean = false
+    private var isLoading = false
+    private var isLastPage = false
+    private var pages = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,14 +46,17 @@ class NewsFragment : Fragment() {
         fragmentNewsBinding.rvNews.apply {
             adapter = newsAdapter
             layoutManager = LinearLayoutManager(activity)
+            addOnScrollListener(this@NewsFragment.onScrollListener)
         }
     }
 
     private fun showProgressBar() {
+        isLoading = true
         fragmentNewsBinding.progressBarNews.visibility = View.VISIBLE
     }
 
     private fun hideProgressBar() {
+        isLoading = false
         fragmentNewsBinding.progressBarNews.visibility = View.INVISIBLE
     }
 
@@ -62,6 +71,12 @@ class NewsFragment : Fragment() {
                     response.data?.let {
                         Log.i("MYTAG", "List of articles ${it.articles.toList().size}")
                         newsAdapter.differ.submitList(it.articles.toList())
+                        if (it.totalResults%20 == 0) {
+                            pages = it.totalResults/20
+                        } else {
+                            pages = it.totalResults/20 + 1
+                        }
+                        isLastPage = page == pages
                     }
                 }
                 is Resource.Error -> {
@@ -78,5 +93,31 @@ class NewsFragment : Fragment() {
 
             }
         })
+    }
+
+    private val onScrollListener = object : RecyclerView.OnScrollListener() {
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+            if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                isScrolling = true
+            }
+        }
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+            val layoutManager = fragmentNewsBinding.rvNews.layoutManager as LinearLayoutManager
+            val sizeOfTheCurrentList = layoutManager.itemCount
+            val visibleItems = layoutManager.childCount
+            val topPosition = layoutManager.findFirstVisibleItemPosition()
+
+            val hasReachedToEnd = topPosition + visibleItems >= sizeOfTheCurrentList
+            val shouldPaginate = !isLoading && !isLastPage && hasReachedToEnd && isScrolling
+
+            if (shouldPaginate) {
+                page++
+                viewModel.getNewsHeadlines(country, page)
+                isScrolling = false
+            }
+        }
     }
 }
